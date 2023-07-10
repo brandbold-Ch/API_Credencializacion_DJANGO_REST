e594528c31997a1646ea09cd2e95456f8ff3cb90
CodeR <118649094+brandbold-Ch@users.noreply.github.com>
2023-07-09 23:48:03 -0600

    Update settings.py

--- Gestor/settings.py
+++ Gestor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import cloudinary
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,9 +106,7 @@ WSGI_APPLICATION = 'Gestor.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 }
 
